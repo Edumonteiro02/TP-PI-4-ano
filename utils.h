6b93fe8386107caa14e7accb8184fb8503f89942
DiogoR4556 <94021147+DiogoR4556@users.noreply.github.com>
2023-12-23 20:43:15 +0000

    Add files via upload

--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #include "fileio.h"
 
+void calculos(Data* circuitos);
 void ordenardados(Data* circuitos);
 void listardados(int op, Data* circuitos);
-void procuranome(char nome[50], Data *circuitos);
-void procurano(char no1, char no2, Data *circuitos);
+void procuranome(char nome[50], Data* circuitos);
+void procurano(char no1, char no2, Data* circuitos);
